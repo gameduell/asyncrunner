f18b169b50b4a4d3b2f08e826336545626f96963
Rui Campos <rui.campos@gameduell.de>
2014-11-26 11:39:32 +0100

    added ci script

---
+++ ci.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+set -e
+
+cd tests
+
+rm -rf Export
+
+expect -c "
+spawn haxelib run duell build android -test -verbose -overridebranch master -D jenkins
+
+set timeout -1
+
+expect {
+	\"is not currently installed.\" {
+
+		send \"y\r\"
+
+		exp_continue
+	}
+	\"is missing,\" {
+
+		send \"y\r\"
+
+		exp_continue
+	}
+	\"not up to date\" {
+
+		send \"y\r\"
+
+		exp_continue
+	}
+}
+"
+
+
+
+expect -c "
+spawn haxelib run duell build html5 -test -verbose -overridebranch master -D jenkins
+
+set timeout -1
+
+expect {
+	\"is not currently installed.\" {
+
+		send \"y\r\"
+
+		exp_continue
+	}
+	\"is missing,\" {
+
+		send \"y\r\"
+
+		exp_continue
+	}
+	\"not up to date\" {
+
+		send \"y\r\"
+
+		exp_continue
+	}
+}
+"
+
+
+expect -c "
+spawn haxelib run duell build flash -test -verbose -overridebranch master -D jenkins
+
+set timeout -1
+
+expect {
+	\"is not currently installed.\" {
+
+		send \"y\r\"
+
+		exp_continue
+	}
+	\"is missing,\" {
+
+		send \"y\r\"
+
+		exp_continue
+	}
+	\"not up to date\" {
+
+		send \"y\r\"
+
+		exp_continue
+	}
+}
+"
+
+expect -c "
+spawn haxelib run duell build ios -test -verbose -overridebranch master -D jenkins
+
+set timeout -1
+
+expect {
+	\"is not currently installed.\" {
+
+		send \"y\r\"
+
+		exp_continue
+	}
+	\"is missing,\" {
+
+		send \"y\r\"
+
+		exp_continue
+	}
+	\"not up to date\" {
+
+		send \"y\r\"
+
+		exp_continue
+	}
+}
+"
+
+
+
