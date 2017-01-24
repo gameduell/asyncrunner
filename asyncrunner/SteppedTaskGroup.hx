a48a459741a88c7e1396705227499b51837fe326
Ricardo Silva <ricardo.silva@studio49.co>
2017-01-24 15:42:47 +0100

    created stepped task group task

---
+++ asyncrunner/SteppedTaskGroup.hx
@@ -0,0 +1,80 @@
+package asyncrunner;
+
+import asyncrunner.Task;
+
+class SteppedTaskGroup extends SequentialTaskGroup
+{
+    public var stepTasks (default, null): Array<Task>;
+
+    private var blockingTasks: Array<BlockingTask>;
+    private var currentStepIndex: Int;
+
+    public function new(tasks: Array<Task>) : Void
+    {
+        currentStepIndex = 0;
+        stepTasks = [];
+        blockingTasks = [];
+
+        var tasksToRun: Array<Task> = [];
+        for (i in 0 ... tasks.length)
+        {
+            stepTasks[i] = tasks[i];
+            tasksToRun.push(stepTasks[i]);
+
+            blockingTasks[i] = new BlockingTask();
+            tasksToRun.push(blockingTasks[i]);
+        }
+
+        super(tasksToRun);
+    }
+
+    override public function cancel(): Void
+    {
+        currentStepIndex = 0;
+
+        super.cancel();
+    }
+
+    override public function fail(?failureCode: Int, ?failureMessage: String): Void
+    {
+        currentStepIndex = 0;
+
+        super.fail(failureCode, failureMessage);
+    }
+
+    override public function finish(): Void
+    {
+        currentStepIndex = 0;
+
+        super.finish();
+    }
+
+    public function unblockAllSteps(): Void
+    {
+        for (i in 0 ... blockingTasks.length)
+        {
+            blockingTasks[i].unblock();
+        }
+    }
+
+    public function unblockStepAtIndex(index: Int): Void
+    {
+        if (index >= blockingTasks.length)
+        {
+            return;
+        }
+
+        blockingTasks[index].unblock();
+    }
+
+    public function unblockNextStep(): Void
+    {
+        if (currentStepIndex >= blockingTasks.length)
+        {
+            return;
+        }
+
+        blockingTasks[currentStepIndex].unblock();
+        ++currentStepIndex;
+    }
+}
